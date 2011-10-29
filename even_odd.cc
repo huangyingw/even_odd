8d246c6fb0f462715664dbcc79883cfef7ad0ac1
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-29 17:35:30 +0800

    n

--- even_odd.cc
+++ even_odd.cc
@@ -33,26 +33,31 @@ int f(int a[ ], int len)
 
 int main() 
 {
-  int a[] = {4, 2, 5};//4
+  int a[] = {1};//1
   cout<<f(a,sizeof(a)/sizeof(int))<<endl;
 
-  int b[]={1, 2, 3, 4};//3
+  int b[]={1, 2};//-1
   cout<<f(b,sizeof(b)/sizeof(int))<<endl;
 
-  int c[]={4, 1, 2, 3};//3
+  int c[]={1, 2, 3};//2
   cout<<f(c,sizeof(c)/sizeof(int))<<endl;
 
-  int d[]={1, 1, 2, 2};//1
+  int d[]={1, 2, 3, 4};//-2
   cout<<f(d,sizeof(d)/sizeof(int))<<endl;
 
-  int e[]={1, 1};//-1
+  int e[]={3, 3, 4, 4};//-2
   cout<<f(e,sizeof(e)/sizeof(int))<<endl;
 
-  int h[]={1};//-1
+  int h[]={3, 2, 3, 4};//0
   cout<<f(h,sizeof(h)/sizeof(int))<<endl;
 
-  int g[]={};//-1
+  int g[]={4, 1, 2, 3};//-2
   cout<<f(g,sizeof(g)/sizeof(int))<<endl;
 
+  int j[]={1, 1};//2
+  cout<<f(j,sizeof(j)/sizeof(int))<<endl;
+
+  int i[]={};//0
+  cout<<f(i,sizeof(i)/sizeof(int))<<endl;
   return 0;
 }
